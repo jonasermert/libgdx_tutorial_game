f844e56dbc215769720e363415dfa1ee9d7ef66c
pilzschaf <pilzschaf@t-online.de>
2019-08-30 19:46:49 +0200

    Initial commit

---
+++ desktop/src/de/pilzschaf/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.pilzschaf.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.pilzschaf.game.TutorialGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TutorialGame(), config);
+	}
+}
