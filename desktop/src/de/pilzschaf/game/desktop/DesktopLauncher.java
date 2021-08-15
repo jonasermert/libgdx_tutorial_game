528ad9a5e9e2c65195c58f7d5372343fef56c4e8
Jonas Ermert <ermert.jonas@yahoo.de>
2021-08-15 08:47:32 +0200

    Config changed in DesktopLauncher.java

--- desktop/src/de/pilzschaf/game/desktop/DesktopLauncher.java
+++ desktop/src/de/pilzschaf/game/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package de.pilzschaf.game.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import de.pilzschaf.game.TutorialGame;
@@ -7,6 +8,15 @@ import de.pilzschaf.game.TutorialGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = "Tutorial Game";
+		config.width = 800;
+		config.height = 480;
+		config.addIcon("data/ic_launcher.png", Files.FileType.Internal);
+
+
+
+
 		new LwjglApplication(new TutorialGame(), config);
 	}
 }
